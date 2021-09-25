642864bf1ee4ef6ef8065fb4aab0aac8c3894cc4
Theminox <Manuelgamer1913@gmail.com>
2021-09-25 10:11:32 -0500

    New

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  peliculasNuevas: Pelicula[] = [];
+
+  constructor( private movieService: MoviesService) {}
+
+  ngOnInit(){
+    this.movieService.getFeature()
+    .subscribe(resp => {
+      this.peliculasNuevas = resp.results;
+    });
+
+  }
+}
